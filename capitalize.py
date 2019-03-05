61e2de080caa64af8a2eb899dac1ac944504af5c
DAisyNim <48229457+DAisyNim@users.noreply.github.com>
2019-03-05 11:02:02 +0900

    Create capitalize.py

---
+++ capitalize.py
@@ -0,0 +1,62 @@
+def capitalize(a):
+    """ (str) -> str
+    """
+    if a[0] == 'a':
+        b= a.replace(a[0],'A')
+    elif a[0] == 'b':
+        b= a.replace(a[0],'B')
+    elif a[0] == 'c':
+        b= a.replace(a[0],'C')
+    elif a[0] == 'd':
+        b= a.replace(a[0],'D')
+    elif a[0] == 'e':
+        b= a.replace(a[0],'E')
+    elif a[0] == 'f':
+        b= a.replace(a[0],'F')
+    elif a[0] == 'g':
+        b= a.replace(a[0],'G')
+    elif a[0] == 'h':
+        b= a.replace(a[0],'H')
+    elif a[0] == 'i':
+        b= a.replace(a[0],'I')
+    elif a[0] == 'j':
+        b= a.replace(a[0],'J')
+    elif a[0] == 'k':
+        b= a.replace(a[0],'K')
+    elif a[0] == 'l':
+        b= a.replace(a[0],'L')
+    elif a[0] == 'm':
+        b= a.replace(a[0],'M')
+    elif a[0] == 'n':
+        b= a.replace(a[0],'N')
+    elif a[0] == 'o':
+        b= a.replace(a[0],'O')
+    elif a[0] == 'p':
+        b= a.replace(a[0],'P')
+    elif a[0] == 'q':
+        b= a.replace(a[0],'Q')
+    elif a[0] == 'r':
+        b= a.replace(a[0],'R')
+    elif a[0] == 's':
+        b= a.replace(a[0],'S')
+    elif a[0] == 't':
+        b= a.replace(a[0],'T')
+    elif a[0] == 'u':
+        b= a.replace(a[0],'U')
+    elif a[0] == 'v':
+        b= a.replace(a[0],'V')
+    elif a[0] == 'w':
+        b= a.replace(a[0],'W')
+    elif a[0] == 'x':
+        b= a.replace(a[0],'X')
+    elif a[0] == 'y':
+        b= a.replace(a[0],'Y')
+    elif a[0] == 'z':
+        b= a.replace(a[0],'Z')
+    else: 
+        b="영어로 입력하세요."
+    
+    return (print(b))
+
+print(capitalize('apple'))
+
